cc81e1802a6ca85ddff65a42fac3098bce64a6e3
GuadaCiberneticaAhreQueDecia <164645806+GuadisPrograma@users.noreply.github.com>
2024-04-25 08:18:05 -0300

    Add files via upload
    profe Ale dsp t los entrego completo en ccasaa

---
+++ index.js
@@ -0,0 +1,8 @@
+let UsuarioNombre = 'JuanGarcia23'
+let ContraseñaNombre = 'cJZoht3!'
+// variable valorFIJO
+if ((UsuarioNombre== 'JuanGarcia23') && (ContraseñaNombre == 'cJZoht3!')) {
+    alert('hola podes entrar')
+} else {
+    alert('no podes entrar')
+}
