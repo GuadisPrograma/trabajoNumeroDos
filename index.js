c6792da334cc1a8a5731ffe759a4fcee3739a8d0
GuadaCiberneticaAhreQueDecia <164645806+GuadisPrograma@users.noreply.github.com>
2024-05-02 08:19:34 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -6,3 +6,23 @@ if ((UsuarioNombre== 'JuanGarcia23') && (ContraseñaNombre == 'cJZoht3!')) {
 } else {
     alert('no podes entrar')
 }
+//actividad 2
+let NotaExamen = 8
+let NotaDeTrabajosPracticos = 6
+let Asistenica = true 
+if ((NotaExamen <=8) && (NotaDeTrabajosPracticos <= 6) || (Asistenica == true)){
+    alert('aprobaste la materia!!¡¡')
+
+}else{
+    alert('desaprobaste la materia.')
+}
+
+//actividad tres 
+let día = prompt("que día es hoy?")
+if ((día == "lunes") || (día == "miercoles") || ( día == 'viernes') ) {
+    alert('pasará el camión de residuos humedos')
+} else if (día == 'sabado') {
+    alert('no pasará')
+}else{ 
+     alert('pasará el seco')
+}
